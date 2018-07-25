5fbb388802d8ab37dae1939d1aafed3018b9c531
Pascal Baillehache <30551642+BayashiPascal@users.noreply.github.com>
2018-07-25 22:46:22 +0900

    Added time measurement

--- Examples/Template/main.c
+++ Examples/Template/main.c
@@ -155,6 +155,10 @@ NeuraNet* createNN(void) {
 void Learn(DataSetCat cat) {
   // Init the random generator
   srandom(time(NULL));
+  // Declare variables to measure time
+  struct timespec start, stop;
+  // Start measuring time
+  clock_gettime(CLOCK_REALTIME, &start);
   // Load the DataSet
   DataSet* dataset = PBErrMalloc(NeuraNetErr, sizeof(DataSet));
   bool ret = DataSetLoad(dataset, cat);
@@ -233,9 +237,19 @@ void Learn(DataSetCat cat) {
     // If there has been improvement during this epoch
     if (curBest > bestVal) {
       bestVal = curBest;
+      // Measure time
+      clock_gettime(CLOCK_REALTIME, &stop);
+      float elapsed = stop.tv_sec - start.tv_sec;
+      int day = (int)floor(elapsed / 86400);
+      elapsed -= (float)(day * 86400);
+      int hour = (int)floor(elapsed / 3600);
+      elapsed -= (float)(hour * 3600);
+      int min = (int)floor(elapsed / 60);
+      elapsed -= (float)(min * 60);
+      int sec = (int)floor(elapsed);
       // Display info about the improvment
-      printf("\nImprovement at epoch %lu: %f\n", 
-        GAGetCurEpoch(ga), bestVal);
+      printf("Improvement at epoch %lu: %f (in %d:%d:%d:%ds)  \n", 
+        GAGetCurEpoch(ga), bestVal, day, hour, min, sec);
       fflush(stdout);
       // Set the links and base functions of the NeuraNet according
       // to the best adn
@@ -270,7 +284,18 @@ void Learn(DataSetCat cat) {
     int ret = system("mv ./bestga.tmp ./bestga.txt");
     (void)ret;
   }
-  printf("\nLearning complete\n");
+  // Measure time
+  clock_gettime(CLOCK_REALTIME, &stop);
+  float elapsed = stop.tv_sec - start.tv_sec;
+  int day = (int)floor(elapsed / 86400);
+  elapsed -= (float)(day * 86400);
+  int hour = (int)floor(elapsed / 3600);
+  elapsed -= (float)(hour * 3600);
+  int min = (int)floor(elapsed / 60);
+  elapsed -= (float)(min * 60);
+  int sec = (int)floor(elapsed);
+  printf("\nLearning complete (in %d:%d:%d:%ds)\n", 
+    day, hour, min, sec);
   // Free memory
   NeuraNetFree(&nn);
   GenAlgFree(&ga);
@@ -298,6 +323,8 @@ void Check(const NeuraNet* const that, const DataSetCat cat) {
 // 'nbInp' char*)
 void Predict(const NeuraNet* const that, const int nbInp, 
   char** const inputs) {
+  // Start measuring time
+  clock_t clockStart = clock();
   // Check the number of inputs
   if (nbInp != NNGetNbInput(that)) {
     printf("Wrong number of inputs, there should %d, there was %d\n",
@@ -316,8 +343,15 @@ void Predict(const NeuraNet* const that, const int nbInp,
   // Predict
   NNEval(that, input, output);
 
+  // End measuring time
+  clock_t clockEnd = clock();
+  double timeUsed = 
+    ((double)(clockEnd - clockStart)) / (CLOCKS_PER_SEC * 0.001) ;
+  // If the clock has been reset meanwhile
+  if (timeUsed < 0.0)
+    timeUsed = 0.0;
   //if (VecGet(output, 0) == ...)
-  //  printf("...");
+  //  printf("...(in %fms)", timeUsed);
   
   // Free memory
   VecFree(&input);
