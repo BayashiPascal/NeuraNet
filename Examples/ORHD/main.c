43e1676f03c82713a79bb2f1a04322dd50c7bc2d
Pascal Baillehache <30551642+BayashiPascal@users.noreply.github.com>
2018-09-09 14:59:05 +0900

    Add files via upload

--- Examples/ORHD/main.c
+++ Examples/ORHD/main.c
@@ -20,8 +20,8 @@
 #define NB_INPUT 64
 #define NB_OUTPUT 10
 // Nb max of hidden values, links and base functions
-#define NB_MAXHIDDEN 100
-#define NB_MAXLINK 300
+#define NB_MAXHIDDEN 1
+#define NB_MAXLINK 100
 #define NB_MAXBASE NB_MAXLINK
 // Size of the gene pool and elite pool
 #define ADN_SIZE_POOL 100
@@ -32,11 +32,9 @@
 // Value of the NeuraNet above which the learning process stops
 #define STOP_LEARNING_AT_VAL 0.999
 // Number of epoch above which the learning process stops
-#define STOP_LEARNING_AT_EPOCH 100000
+#define STOP_LEARNING_AT_EPOCH 5000
 // Save NeuraNet in compact format
 #define COMPACT true
-// Use convolution if true
-#define CONVOLUTION false
 
 // Categories of data sets
 
@@ -179,6 +177,10 @@ bool DataSetLoad(DataSet* const that, const DataSetCat cat) {
         that->_samples[iSample]._props + 62,
         that->_samples[iSample]._props + 63,
         &(that->_samples[iSample]._cat));
+      for (int iProp = 64; iProp--;) {
+        that->_samples[iSample]._props[iProp] /= 8.0;
+        that->_samples[iSample]._props[iProp] -= 1.0;
+      }
       if (ret == EOF) {
         printf("Couldn't read the dataset\n");
         fclose(f);
@@ -262,6 +264,180 @@ bool DataSetLoad(DataSet* const that, const DataSetCat cat) {
         that->_samples[iSample]._props + 62,
         that->_samples[iSample]._props + 63,
         &(that->_samples[iSample]._cat));
+      for (int iProp = 64; iProp--;) {
+        that->_samples[iSample]._props[iProp] /= 8.0;
+        that->_samples[iSample]._props[iProp] -= 1.0;
+      }
+      if (ret == EOF) {
+        printf("Couldn't read the dataset\n");
+        fclose(f);
+        return false;
+      }
+    }
+    fclose(f);
+  } else if (cat == dataall) {
+    that->_nbSample = 3823 + 1797;
+    that->_samples = 
+      PBErrMalloc(NeuraNetErr, sizeof(ORHD) * that->_nbSample);
+    FILE* f = fopen("./optdigits.tra", "r");
+    if (f == NULL) {
+      printf("Couldn't open the data set file\n");
+      return false;
+    }
+    for (unsigned int iSample = 0; iSample < 3823; ++iSample) {
+      ret = fscanf(f, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d", 
+        that->_samples[iSample]._props,
+        that->_samples[iSample]._props + 1,
+        that->_samples[iSample]._props + 2,
+        that->_samples[iSample]._props + 3,
+        that->_samples[iSample]._props + 4,
+        that->_samples[iSample]._props + 5,
+        that->_samples[iSample]._props + 6,
+        that->_samples[iSample]._props + 7,
+        that->_samples[iSample]._props + 8,
+        that->_samples[iSample]._props + 9,
+        that->_samples[iSample]._props + 10,
+        that->_samples[iSample]._props + 11,
+        that->_samples[iSample]._props + 12,
+        that->_samples[iSample]._props + 13,
+        that->_samples[iSample]._props + 14,
+        that->_samples[iSample]._props + 15,
+        that->_samples[iSample]._props + 16,
+        that->_samples[iSample]._props + 17,
+        that->_samples[iSample]._props + 18,
+        that->_samples[iSample]._props + 19,
+        that->_samples[iSample]._props + 20,
+        that->_samples[iSample]._props + 21,
+        that->_samples[iSample]._props + 22,
+        that->_samples[iSample]._props + 23,
+        that->_samples[iSample]._props + 24,
+        that->_samples[iSample]._props + 25,
+        that->_samples[iSample]._props + 26,
+        that->_samples[iSample]._props + 27,
+        that->_samples[iSample]._props + 28,
+        that->_samples[iSample]._props + 29,
+        that->_samples[iSample]._props + 30,
+        that->_samples[iSample]._props + 31,
+        that->_samples[iSample]._props + 32,
+        that->_samples[iSample]._props + 33,
+        that->_samples[iSample]._props + 34,
+        that->_samples[iSample]._props + 35,
+        that->_samples[iSample]._props + 36,
+        that->_samples[iSample]._props + 37,
+        that->_samples[iSample]._props + 38,
+        that->_samples[iSample]._props + 39,
+        that->_samples[iSample]._props + 40,
+        that->_samples[iSample]._props + 41,
+        that->_samples[iSample]._props + 42,
+        that->_samples[iSample]._props + 43,
+        that->_samples[iSample]._props + 44,
+        that->_samples[iSample]._props + 45,
+        that->_samples[iSample]._props + 46,
+        that->_samples[iSample]._props + 47,
+        that->_samples[iSample]._props + 48,
+        that->_samples[iSample]._props + 49,
+        that->_samples[iSample]._props + 50,
+        that->_samples[iSample]._props + 51,
+        that->_samples[iSample]._props + 52,
+        that->_samples[iSample]._props + 53,
+        that->_samples[iSample]._props + 54,
+        that->_samples[iSample]._props + 55,
+        that->_samples[iSample]._props + 56,
+        that->_samples[iSample]._props + 57,
+        that->_samples[iSample]._props + 58,
+        that->_samples[iSample]._props + 59,
+        that->_samples[iSample]._props + 60,
+        that->_samples[iSample]._props + 61,
+        that->_samples[iSample]._props + 62,
+        that->_samples[iSample]._props + 63,
+        &(that->_samples[iSample]._cat));
+      for (int iProp = 64; iProp--;) {
+        that->_samples[iSample]._props[iProp] /= 8.0;
+        that->_samples[iSample]._props[iProp] -= 1.0;
+      }
+      if (ret == EOF) {
+        printf("Couldn't read the dataset\n");
+        fclose(f);
+        return false;
+      }
+    }
+    fclose(f);
+    f = fopen("./optdigits.tes", "r");
+    if (f == NULL) {
+      printf("Couldn't open the data set file\n");
+      return false;
+    }
+    for (unsigned int iSample = 3824; iSample < 3823 + 1797; ++iSample) {
+      ret = fscanf(f, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d", 
+        that->_samples[iSample]._props,
+        that->_samples[iSample]._props + 1,
+        that->_samples[iSample]._props + 2,
+        that->_samples[iSample]._props + 3,
+        that->_samples[iSample]._props + 4,
+        that->_samples[iSample]._props + 5,
+        that->_samples[iSample]._props + 6,
+        that->_samples[iSample]._props + 7,
+        that->_samples[iSample]._props + 8,
+        that->_samples[iSample]._props + 9,
+        that->_samples[iSample]._props + 10,
+        that->_samples[iSample]._props + 11,
+        that->_samples[iSample]._props + 12,
+        that->_samples[iSample]._props + 13,
+        that->_samples[iSample]._props + 14,
+        that->_samples[iSample]._props + 15,
+        that->_samples[iSample]._props + 16,
+        that->_samples[iSample]._props + 17,
+        that->_samples[iSample]._props + 18,
+        that->_samples[iSample]._props + 19,
+        that->_samples[iSample]._props + 20,
+        that->_samples[iSample]._props + 21,
+        that->_samples[iSample]._props + 22,
+        that->_samples[iSample]._props + 23,
+        that->_samples[iSample]._props + 24,
+        that->_samples[iSample]._props + 25,
+        that->_samples[iSample]._props + 26,
+        that->_samples[iSample]._props + 27,
+        that->_samples[iSample]._props + 28,
+        that->_samples[iSample]._props + 29,
+        that->_samples[iSample]._props + 30,
+        that->_samples[iSample]._props + 31,
+        that->_samples[iSample]._props + 32,
+        that->_samples[iSample]._props + 33,
+        that->_samples[iSample]._props + 34,
+        that->_samples[iSample]._props + 35,
+        that->_samples[iSample]._props + 36,
+        that->_samples[iSample]._props + 37,
+        that->_samples[iSample]._props + 38,
+        that->_samples[iSample]._props + 39,
+        that->_samples[iSample]._props + 40,
+        that->_samples[iSample]._props + 41,
+        that->_samples[iSample]._props + 42,
+        that->_samples[iSample]._props + 43,
+        that->_samples[iSample]._props + 44,
+        that->_samples[iSample]._props + 45,
+        that->_samples[iSample]._props + 46,
+        that->_samples[iSample]._props + 47,
+        that->_samples[iSample]._props + 48,
+        that->_samples[iSample]._props + 49,
+        that->_samples[iSample]._props + 50,
+        that->_samples[iSample]._props + 51,
+        that->_samples[iSample]._props + 52,
+        that->_samples[iSample]._props + 53,
+        that->_samples[iSample]._props + 54,
+        that->_samples[iSample]._props + 55,
+        that->_samples[iSample]._props + 56,
+        that->_samples[iSample]._props + 57,
+        that->_samples[iSample]._props + 58,
+        that->_samples[iSample]._props + 59,
+        that->_samples[iSample]._props + 60,
+        that->_samples[iSample]._props + 61,
+        that->_samples[iSample]._props + 62,
+        that->_samples[iSample]._props + 63,
+        &(that->_samples[iSample]._cat));
+      for (int iProp = 64; iProp--;) {
+        that->_samples[iSample]._props[iProp] /= 8.0;
+        that->_samples[iSample]._props[iProp] -= 1.0;
+      }
       if (ret == EOF) {
         printf("Couldn't read the dataset\n");
         fclose(f);
@@ -303,23 +479,20 @@ float Evaluate(const NeuraNet* const that,
   int countOk[NB_OUTPUT] = {0};
   int countNg[NB_OUTPUT] = {0};
   for (unsigned int iSample = dataset->_nbSample; iSample--;) {
-    // batching
-    //if (dataset->_cat != datalearn || rnd() < 0.5) {
-    if (dataset->_cat != datalearn || rnd() < 1.0) {
-      for (int iInp = 0; iInp < NNGetNbInput(that); ++iInp) {
-        VecSet(input, iInp,
-          dataset->_samples[iSample]._props[iInp]);
-      }
-      NNEval(that, input, output);
-      int pred = VecGetIMaxVal(output);
-      ++(countCat[dataset->_samples[iSample]._cat]);
-      if (pred == dataset->_samples[iSample]._cat) {
-        ++(countOk[dataset->_samples[iSample]._cat]);
-      } else if (dataset->_cat == datalearn) {
-        ++(countNg[dataset->_samples[iSample]._cat]);
-      }
+    for (int iInp = 0; iInp < NNGetNbInput(that); ++iInp) {
+      VecSet(input, iInp,
+        dataset->_samples[iSample]._props[iInp]);
+    }
+    NNEval(that, input, output);
+    int pred = VecGetIMaxVal(output);
+    ++(countCat[dataset->_samples[iSample]._cat]);
+    if (pred == dataset->_samples[iSample]._cat) {
+      ++(countOk[dataset->_samples[iSample]._cat]);
+    } else if (dataset->_cat == datalearn) {
+      ++(countNg[dataset->_samples[iSample]._cat]);
     }
   }
+
   int nbCat = 0;
   for (int iCat = 0; iCat < NB_OUTPUT; ++iCat) {
     if (countCat[iCat] > 0) {
@@ -337,6 +510,7 @@ float Evaluate(const NeuraNet* const that,
       }
     }
   }
+
   if (dataset->_cat != datalearn)
     val /= (float)(dataset->_nbSample);
   else
@@ -351,28 +525,6 @@ float Evaluate(const NeuraNet* const that,
 
 // Create the NeuraNet
 NeuraNet* createNN(void) {
-#if CONVOLUTION
-  // Create the NeuraNet
-  int nbOut = NB_OUTPUT;
-  int thickConv = 10;
-  int depthConv = 2;
-  VecShort* dimIn = VecShortCreate(2);
-  VecSet(dimIn, 0, 8);
-  VecSet(dimIn, 1, 8);
-  VecShort* dimCell = VecShortCreate(2);
-  VecSet(dimCell, 0, 4);
-  VecSet(dimCell, 1, 4);
-  printf("Creating convoluted NeuraNet...\n");
-  fflush(stdout);
-  NeuraNet* nn = NeuraNetCreateConvolution(dimIn, nbOut, dimCell, 
-    depthConv, thickConv);
-  printf("Created convoluted NeuraNet\n");
-  printf("%ld links, %ld bases\n", NNGetNbMaxLinks(nn), 
-    NNGetNbMaxBases(nn));
-  fflush(stdout);
-  // Return the NeuraNet
-  return nn;
-#else
   // Create the NeuraNet
   int nbIn = NB_INPUT;
   int nbOut = NB_OUTPUT;
@@ -383,10 +535,9 @@ NeuraNet* createNN(void) {
     NeuraNetCreate(nbIn, nbOut, nbMaxHid, nbMaxBase, nbMaxLink);
   // Return the NeuraNet
   return nn;
-#endif
 }
 
-// Learn based on the SataSetCat 'cat'
+// Learn based on the DataSetCat 'cat'
 void Learn(DataSetCat cat) {
   // Init the random generator
   srandom(time(NULL));
@@ -422,10 +573,8 @@ void Learn(DataSetCat cat) {
       printf("Previous GenAlg reloaded.\n");
       if (GABestAdnF(ga) != NULL)
         NNSetBases(nn, GABestAdnF(ga));
-#if CONVOLUTION == false
       if (GABestAdnI(ga) != NULL)
         NNSetLinks(nn, GABestAdnI(ga));
-#endif
       bestVal = Evaluate(nn, dataset);
       printf("Starting with best at %f.\n", bestVal);
       limitEpoch += GAGetCurEpoch(ga);
@@ -434,24 +583,13 @@ void Learn(DataSetCat cat) {
   } else {
     printf("Creating new GenAlg...\n");
     fflush(stdout);
-#if CONVOLUTION
-    ga = GenAlgCreate(ADN_SIZE_POOL, ADN_SIZE_ELITE, 
-      NNGetGAAdnFloatLength(nn), 0);
-    NNSetGABoundsBases(nn, ga);
-#else
     ga = GenAlgCreate(ADN_SIZE_POOL, ADN_SIZE_ELITE, 
       NNGetGAAdnFloatLength(nn), NNGetGAAdnIntLength(nn));
     NNSetGABoundsBases(nn, ga);
     NNSetGABoundsLinks(nn, ga);
-#endif
     // Must be declared as a GenAlg applied to a NeuraNet with 
     // convolution
-#if CONVOLUTION
-    GASetTypeNeuraNetConv(ga, NB_INPUT, NNGetNbMaxHidden(nn), NB_OUTPUT,
-      NNGetNbBasesConv(nn), NNGetNbBasesCellConv(nn));
-#else
     GASetTypeNeuraNet(ga, NB_INPUT, NB_MAXHIDDEN, NB_OUTPUT);
-#endif
     GAInit(ga);
   }
   // If there is a NeuraNet available, reload it into the GenAlg
@@ -468,8 +606,10 @@ void Learn(DataSetCat cat) {
       bestVal = Evaluate(nn, dataset);
       printf("Starting with best at %f.\n", bestVal);
       GenAlgAdn* adn = GAAdn(ga, 0);
-      VecCopy(adn->_adnF, nn->_bases);
-      VecCopy(adn->_adnI, nn->_links);
+      if (adn->_adnF)
+        VecCopy(adn->_adnF, nn->_bases);
+      if (adn->_adnI)
+        VecCopy(adn->_adnI, nn->_links);
     }
     fclose(fd);
   }
@@ -500,10 +640,8 @@ void Learn(DataSetCat cat) {
       // to this adn
       if (GABestAdnF(ga) != NULL)
         NNSetBases(nn, GAAdnAdnF(adn));
-#if CONVOLUTION == false
       if (GABestAdnI(ga) != NULL)
         NNSetLinks(nn, GAAdnAdnI(adn));
-#endif
       // Evaluate the NeuraNet
       float value = Evaluate(nn, dataset);
       // Update the value of this adn
@@ -539,10 +677,8 @@ void Learn(DataSetCat cat) {
       GenAlgAdn* bestAdn = GAAdn(ga, curBestI);
       if (GAAdnAdnF(bestAdn) != NULL)
         NNSetBases(nn, GAAdnAdnF(bestAdn));
-#if CONVOLUTION == false
       if (GAAdnAdnI(bestAdn) != NULL)
         NNSetLinks(nn, GAAdnAdnI(bestAdn));
-#endif
       // Save the best NeuraNet
       fd = fopen("./bestnn.txt", "w");
       if (!NNSave(nn, fd, COMPACT)) {
@@ -595,7 +731,7 @@ void Learn(DataSetCat cat) {
   int sec = (int)floor(elapsed);
   printf("\nLearning complete (in %d:%d:%d:%ds)\n", 
     day, hour, min, sec);
-  fflush(stdout);  
+  fflush(stdout);
   // Free memory
   NeuraNetFree(&nn);
   GenAlgFree(&ga);
