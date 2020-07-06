6178258b3db4b51ff8b869d216251cd2b0c5dcd5
BayashiPascal <bayashipascal@gmail.com>
2020-07-06 10:32:26 +0900

    Add conversion data to json for Iris example

--- Examples/Iris/main.c
+++ Examples/Iris/main.c
@@ -532,6 +532,10 @@ int main(int argc, char** argv) {
     } else if (strcmp(argv[1], "-predict") == 0) {
       mode = 2;
     }
+  } else if (argc >= 2) {
+    if (strcmp(argv[1], "-convert") == 0) {
+      mode = 3;
+    }
   }
   // If the mode is invalid print some help
   if (mode == -1) {
@@ -539,6 +543,7 @@ int main(int argc, char** argv) {
     printf("-learn <dataset name>\n");
     printf("-check <dataset name>\n");
     printf("-predict <input values>\n");
+    printf("-convert\n");
     return 0;
   }
   if (mode == 0) {
@@ -563,6 +568,51 @@ int main(int argc, char** argv) {
     fclose(fd);
     Predict(nn, argc - 2, argv + 2);
     NeuraNetFree(&nn);
+  } else if (mode == 3) {
+    DataSet* dataset = PBErrMalloc(NeuraNetErr, sizeof(DataSet));
+    bool ret = DataSetLoad(dataset, dataall);
+    if (!ret) {
+      printf("Couldn't load the data\n");
+      return 1;
+    }
+    FILE* fp = fopen("./iris.json", "w");
+    fprintf(fp,
+"{\n \
+  \"dataSet\": \"Iris dataset\",\n \
+  \"dataSetType\": \"0\",\n \
+  \"desc\": \"Iris dataset, 4 inputs, 3 output\",\n \
+  \"dim\": {\n \
+    \"_dim\":\"1\",\n \
+    \"_val\":[\"7\"]\n \
+  },\n \
+  \"nbSample\": \"%d\",\n \
+  \"samples\": [\n", dataset->_nbSample);
+    for (int iSample = 0;
+      iSample < dataset->_nbSample;
+      ++iSample) {
+        fprintf(fp,
+"    {\n \
+      \"_dim\":\"7\",\n \
+      \"_val\":[\"%f\", \"%f\", \"%f\", \"%f\", \"%f\", \"%f\", \
+      \"%f\"]\n \
+   }",
+      dataset->_samples[iSample]._props[0],
+      dataset->_samples[iSample]._props[1],
+      dataset->_samples[iSample]._props[2],
+      dataset->_samples[iSample]._props[3],
+      (dataset->_samples[iSample]._cat == setosa ? 1.0 : -1.0),
+      (dataset->_samples[iSample]._cat == versicolor ? 1.0 : -1.0),
+      (dataset->_samples[iSample]._cat == virginica ? 1.0 : -1.0));
+
+      if (iSample < dataset->_nbSample - 1)
+        fprintf(fp, ",");
+      fprintf(fp, "\n");
+    }
+    fprintf(fp,
+"  ]\n" \
+"}\n");
+    fclose(fp);
+    DataSetFree(&dataset);
   }
   // Return success code
   return 0;
